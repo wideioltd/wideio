bc84a0dca9f898c5dec7d487060b6a60763560a5
Bertrand Nouvel <bn@wide.io>
2016-05-18 00:55:16 +0100

    fixed git flow hook cfg

--- .git_hooks/git-flow-hooks-config.sh
+++ .git_hooks/git-flow-hooks-config.sh
@@ -1,5 +1,5 @@
 VERSION_FILE="VERSION"
-_VERSION_FILE="$(for d in . .. ../..; do[ -f $d/setup.py ] | grep ^VERSION_FILE $d/setup.py; done)"
+_VERSION_FILE="$(for d in . .. ../..; do [ -f $d/setup.py ] && grep ^VERSION_FILE $d/setup.py; done)"
 if [ "$_VERSION_FILE" ]; then
   VERSION_FILE=$_VERSION_FILE
 fi
